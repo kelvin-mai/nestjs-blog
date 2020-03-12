8fa9de118dec1c5e1445ee5be77456907dd39364
Kelvin Mai <kelvin.mai002@gmail.com>
2020-03-11 19:50:12 -0700

    created database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConnectionService } from './database-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database-connection.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConnectionService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      name: 'default',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+      entities: ['dist/**/*.entity.js'],
+    };
+  }
+}
